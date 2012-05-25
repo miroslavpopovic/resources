9ab10893b50d04206a1f0219e8f469cfd3f6e5ae
Miroslav Popovic <miroslav.popovic@gmail.com>
2012-05-25 22:01:38 +0200

    Started adding some base styles and content

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,25 @@
-/* Author:
-
+/* Author: Miroslav Popovic
 */
+
+(function(){
+
+    $.expr[':'].external = function (obj) {
+        return !(obj.href == "")
+            && !obj.href.match(/^mailto:/)
+            && !(obj.hostname == location.hostname);
+    };
+
+    function enableNewWindowLinks() {
+        $('.main a:external').addClass('new-window');
+
+        $('a.new-window').click(function () {
+            window.open(this.href);
+            return false;
+        });
+    }
+
+    enableNewWindowLinks();
+})();
 
 
 
